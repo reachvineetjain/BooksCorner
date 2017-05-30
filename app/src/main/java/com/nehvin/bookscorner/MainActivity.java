e983eac2f9e015d336cb67a5073ba6268460d6ed
reachvineetjain <vkjain@gmail.com>
2017-05-30 08:24:22 +0530

    Books Corner
    This is a project in one of the Udacity courses on Networking. Have only
    created the Android Studio project and checked it in. Need to work on it
    as per guidelines given

---
+++ app/src/main/java/com/nehvin/bookscorner/MainActivity.java
@@ -0,0 +1,16 @@
+package com.nehvin.bookscorner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+//https://review.udacity.com/#!/rubrics/164/view
+//https://classroom.udacity.com/courses/ud843/lessons/c013e654-996a-41f6-92ee-5a303e74e8f7/concepts/ace02bc1-ca18-4a41-8416-3fb8019a5f84
